279e535730ee5dcf7f954178b5b5965b34a0edd7
3020 <fitsumcs@gmail.com>
2020-10-05 18:25:48 +0300

    Project scafolding 1

---
+++ index.js
@@ -0,0 +1,24 @@
+//packages
+const path = require('path');
+const express = require('express');
+
+//consts 
+const port = process.env.PORT || 3000;
+const publicFolder = path.join(__dirname, './public');
+
+// initialize express
+const app = express();
+
+// config 
+app.use(express.static(publicFolder));
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log("Server Started !!"));
