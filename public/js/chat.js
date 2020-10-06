b94e08664a8bcddce526c1afd1067cd3d1873956
3020 <fitsumcs@gmail.com>
2020-10-06 19:07:58 +0300

    Scroll effect added

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,16 +12,40 @@ const userList_template = document.querySelector('#userList_template').innerHTML
 //query string 
 const { username, room_name } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
+//auto scroll 
+function autoScroll() {
 
+    // new message 
+    const newMessage = messages.lastElementChild;
+
+    //height of new message 
+    const newMessageHeight = newMessage.offsetHeight + parseInt(getComputedStyle(newMessage).marginBottom);
+
+    // visible height 
+    const visibleHeight = messages.offsetHeight;
+    // container height 
+    const containerHeight = messages.scrollHeight;
+
+    // how far scrolled 
+    const scrollHeight = messages.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollHeight) {
+        messages.scrollTop = messages.scrollHeight;
+    }
+
+
+}
 socket.on('message', (message) => {
 
     const html = Mustache.render(message_template, { username: message.username, message: message.text, createdAt: moment(message.createdAt).format('h:mm a') });
     messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
 });
 socket.on('location_message', (message) => {
 
     const html = Mustache.render(location_template, { username: message.username, ref: message.url, createdAt: moment(message.createdAt).format('h:mm a') });
     messages.insertAdjacentHTML('beforeend', html);
+    autoScroll();
 
 });
 
